e5ee5e125188d2a562fd63cdba331520224a4b4c
NandaaAyyy <ayun5038@gmail.com>
2025-10-26 20:19:42 +0700

    push-1

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'pages/login.dart';
+import 'pages/home.dart';
+import 'pages/profile.dart';
+import 'pages/map.dart';
+import 'pages/history.dart';
+
+void main() {
+  runApp(const SpyKidsApp());
+}
+
+class SpyKidsApp extends StatelessWidget {
+  const SpyKidsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EduTrack',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF3399FF)),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (c) => const LoginPage(),
+        '/home': (c) => const HomeShell(),
+        '/profile': (c) => const ProfilePage(),
+        '/map': (c) => const MapPage(),
+        '/history': (c) => const HistoryPage(),
+      },
+    );
+  }
+}
